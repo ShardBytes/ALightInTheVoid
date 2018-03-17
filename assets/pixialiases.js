cf7d877380f7234b20a68cfc7c3c7f33f697339b
Plasmoxy <sebohacker@gmail.com>
2018-03-17 14:47:00 +0100

    added libs

---
+++ assets/pixialiases.js
@@ -0,0 +1,13 @@
+/* A Small PixiJS aliases snippet by Plasmoxy */
+
+let Application = PIXI.Application,
+    Container = PIXI.Container,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Graphics = PIXI.Graphics,
+    TextureCache = PIXI.utils.TextureCache,
+    Sprite = PIXI.Sprite,
+    Text = PIXI.Text,
+    TextStyle = PIXI.TextStyle,
+    Rectangle = PIXI.Rectangle
+;
