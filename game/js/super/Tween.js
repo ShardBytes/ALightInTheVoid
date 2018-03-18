05e625df8fb4c67deada6a60429dc62ca7ad9929
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-03-18 19:29:05 +0100

    mod text, add tween (in progress)

---
+++ game/js/super/Tween.js
@@ -0,0 +1,12 @@
+// this is just sooo amazing. it can animate anything. wow.
+// i'll leave it to linear for window
+
+class Tween {
+
+  constructor(property, initVal, speed) {
+    this.p = property; // property to influence
+    this.v = initVal; // value to change
+    this.s = speed; // speed of change [unit per second]
+  }
+
+}
