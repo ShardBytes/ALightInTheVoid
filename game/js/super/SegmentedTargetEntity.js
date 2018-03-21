b1789e6ced4df884aba1317f4d7233f4edf3b650
Plasmoxy <sebohacker@gmail.com>
2018-03-21 15:21:28 +0100

    upd

--- game/js/super/SegmentedTargetEntity.js
+++ game/js/super/SegmentedTargetEntity.js
@@ -5,6 +5,7 @@
 // ratio -> if this is big, it will quickly move to the target
 // with small ratio, it moves smoother but is more "behind" the target
 // this can act as interpolation, only it is a bit behind, but with low ping it's not so significant.
+// -> by shooting position segments to it, it will fill the positions between segments
 
 class SegmentedTargetEntity extends Entity {
 
