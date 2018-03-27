971057602c1fc9d4039ca58e0fc875c6722830b1
Plasmoxy <sebohacker@gmail.com>
2018-03-27 15:55:44 +0200

    perhaps a good info

--- game/js/gui/BigInfo.js
+++ game/js/gui/BigInfo.js
@@ -4,18 +4,19 @@ class BigInfo extends Text {
   constructor() {
     super('', {
       fontFamily : 'Consolas, Arial',
-      fontSize: 60,
+      fontSize: 30,
       fill : 0x1fee46,
       fontWeight: 'bold',
       align : 'center'
     });
+    this.texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
     this.anchor.set(0.5, 0.5);
     this.align();
   }
 
   align() {
-    this.position.x = app.renderer.width/2;
-    this.position.y = app.renderer.height/2;
+    this.position.x = app.renderer.width*0.5;
+    this.position.y = app.renderer.height*0.7;
   }
 
 }
