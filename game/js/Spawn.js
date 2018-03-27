504ec2d9ec9a47ba8484f0daf0e1346e5edf5273
Plasmoxy <sebohacker@gmail.com>
2018-03-27 21:52:18 +0200

    spawn anim speed

--- game/js/Spawn.js
+++ game/js/Spawn.js
@@ -5,12 +5,12 @@ class Spawn extends Entity {
   constructor(team) {
     if (team == '1') {
       super('spawn1');
-      this.sprite = new Apparition(this, 'blueportal_particles ', '.ase', 35, 0, 0, 10, 1, true, true);
+      this.sprite = new Apparition(this, 'blueportal_particles ', '.ase', 35, 0, 0, 10, 0.5, true, true);
       this.x = -3250;
       this.y = 800;
     } else {
       super('spawn2');
-      this.sprite = new Apparition(this, 'blueportal_particles ', '.ase', 35, 0, 0, 10, 1, true, true);
+      this.sprite = new Apparition(this, 'blueportal_particles ', '.ase', 35, 0, 0, 10, 0.5, true, true);
       this.x = 3250;
       this.y = -800;
     }
