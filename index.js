d13eb26ca4b4e5250600c17c6c8b5fc6cea95a74
Plasmoxy <sebohacker@gmail.com>
2018-03-30 20:29:23 +0200

    logo change

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const DEVELOPMENT_MODE = true;
 
 $(function (){
 
-  /* resize logo on mobile (so pixels are fitted directly and not scaled that much) */
-  if ( $('#logoimgroot').width() < 1024) $('#logoimg').attr('src', '/assets/img/logo512.png');
-
   /* setup buttons and launch */
 
   let start = (name, team) => {
